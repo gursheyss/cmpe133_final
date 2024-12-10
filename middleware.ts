d17848897ac99a52da3e50dfe8d9e0bd9ad1e259
gursheyss <rkwarya@gmail.com>
2024-12-10 09:32:50 -0800

    oops

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ export const config = {
 };
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
+  if (!req.auth && req.nextUrl.pathname !== "/auth") {
+    const newUrl = new URL("/auth", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
